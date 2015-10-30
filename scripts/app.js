9059b2e4b5881c3d7e28055d325b8b13a34576fc
Isaac Bueno <isaacmabueno@gmail.com>
2015-10-29 22:40:08 -0500

    signup, login and dashboard created

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,52 @@ $(document).ready(function(){
 });
 //Initializing the app
 var app = angular.module('bowlsToTheWall', []);
+var client = new BowlingApiClient('http://bowling-api.nextcapital.com/api');
 // setting up a controller
-app.controller("bowlingController", function($scope) {
-  $scope.x="balls to the wallzzzzzz"
-});
+app.controller("bowlingController", ['$scope', function($scope) {
+//anything with $scope can be accessed in the HTML page. This is the purpose of SCOPE
+  $scope.showLoginPage=true;
+  $scope.showSignUpPage=false;
+  $scope.showDashboard=false;
+  $scope.loginEmail="";
+  $scope.loginPassword="";
+  $scope.signUpEmail="";
+  $scope.signUpPassword="";
+  // access to these variables inside the $scope function. I can access the API in this function
+  $scope.loginSubmit = function() {
+    client.loginUser({
+      email: $scope.loginEmail,
+      password: $scope.loginPassword,
+      success: function(user) {
+        //when they login successfully with the right credentials they get a dashboard page
+        console.log(user);
+        $scope.showSignUpPage=false;
+        $scope.showLoginPage=false;
+        $scope.showDashboard=true;
+      },
+      error: function(xhr)  {
+        console.log(JSON.parse(xhr.responseText));
+      }
+    });
+  };
+  $scope.signUpSubmit = function() {
+    client.createUser({
+      email: $scope.signUpEmail,
+      password: $scope.signUpPassword,
+      success: function(user) {
+        console.log(user);
+      },
+      error: function(xhr)  {
+        console.log(JSON.parse(xhr.responseText));
+      }
+      });
+  };
+  $scope.createAccount = function() {
+  $scope.showSignUpPage=true;
+  $scope.showLoginPage=false;
+  };
+  $scope.hasAccount = function() {
+    $scope.showLoginPage=true;
+    $scope.showSignUpPage=false;
+  };
+}]);
